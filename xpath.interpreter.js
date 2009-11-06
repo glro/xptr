a6c63ee571915332ec907ec47d71a06b905e9e33
Tom Switzer <thomas.switzer@gmail.com>
2009-11-06 00:09:18 -0500

    Added dashed-axis name guides to intrepter axis guides

--- xpath.interpreter.js
+++ xpath.interpreter.js
@@ -11,34 +11,44 @@
  */
 (function() {
 
-var xpath = window.xpath;
+var xpath = window.xpath || {};
 var interpreter = xpath.interpreter = {};
 
 var extend = xpath.util.extend;
 var Class = xpath.util.Class;
 
+
 /**
  * Axis guides are functions that take 2 arguments; a node and a callback. A
  * guide for an axis will traverse, in order, all the nodes in that axis, each
  * time calling the callback provided with the current node as the argument.
+ *
  * Generally, we traverse the DOM in document order (pre-order). The 
  * exceptions to this are {@code preceding}, {@code precedingSibling}, 
- * {@code ancestor}, {@code ancestor-or-self} and {@code attribute}. 
- * {@code preceding}, {@code precedingSibling}, {@code ancestor} and
- * {@code ancestor-or-self} traverse the DOM in reverse document order. For 
- * {@code ancestor} and {@code ancestor-or-self} this means we climb up the DOM.
- * The order for {@code attribute} is not guaranteed and the order should be
- * considered arbitrary. If at anytime during the traversal the callback 
- * function returns false, and only false (other values that evaluate to false, 
- * like null or '', won't affect the traversal), the guide will stop the
- * traversal at that node and return.
+ * {@code ancestor}, {@code ancestor-or-self}, {@code attribute} and 
+ * {@code namespace}. {@code preceding}, {@code precedingSibling}, 
+ * {@code ancestor} and {@code ancestor-or-self} traverse the DOM in reverse 
+ * document order. For {@code ancestor} and {@code ancestor-or-self} this means
+ * we climb up the DOM. The order for {@code attribute} and {@code namespace} is
+ * not guaranteed and the order should be considered arbitrary. 
+ * 
+ * If at anytime during the traversal the callback function returns false, and 
+ * only false (other values that evaluate to false, like null or '', won't 
+ * affect the traversal), the guide will stop the traversal at that node and 
+ * return.
+ *
+ * @note When there are axes who have dashes (-) in the name, there is also a
+ * corresponding guide with the dashes removed and camel-cased so it can be used
+ * as an object method (ie. with a .). For example, there is a guide 
+ * {@code guide['descendant-or-self'] and, also, {@code guide.descendantOrSelf}.
  */
-var guide = {
+var guide = xpath.interpreter.guide = {
     self: function(n, cb) {
         cb(n);
     },
     parent: function(n, cb) {
-        cb(n.parentNode);
+        if (n.parentNode)
+            cb(n.parentNode);
     },
     child: function(n, cb) {
         var kids = n.childNodes;
@@ -104,11 +114,15 @@ var guide = {
     reverseOrderDescendant: function(n, cb) {
         /// @todo Re-write this function to be iterative, not recursive...
         
-        if (n.lastChild)
+        if (n.hasChildNodes())
             for (var k = n.lastChild; k; k = k.previousSibling)
                 guide.reverseDescendant(k);
         cb(n);
-    }
+    },
+    'following-sibling': guide.followingSibling,
+    'previous-sibling': guide.previousSibling,
+    'ancestor-or-self': guide.ancestorOrSelf,
+    'descendant-or-self': guide.descendantOrSelf
 };
 
 })();
