fb8c2b024e3ab8ebf8058a44235621aa35347984
Tom Switzer <thomas.switzer@gmail.com>
2010-03-09 23:33:29 -0500

    Moved axis guide to xpath.core.

--- src/xpath.core.js
+++ src/xpath.core.js
@@ -437,4 +437,148 @@ function testEquality(context, equality, nodes, val, reverse) {
                 return false;
         });
 }
+
+
+xpath.core.AxisGuideManager = xpath.util.Class({
+    init: function() {
+        this.axisGuide = new xpath.core.AxisGuide();
+    },
+    
+    getAxisGuide: function(axis) {
+        if (!this.axisGuide[axis])
+            return null;
+
+        var axisGuide = this.axisGuide;
+        return function(n, cb) {
+            axisGuide[axis](n, cb);
+        };
+    }
+});
+
+/**
+ * Axis guides are methods that take 2 arguments; a node and a callback. A
+ * guide for an axis will traverse, in order, all the nodes in that axis, each
+ * time calling the callback provided with the current node as the argument.
+ *
+ * Generally, we traverse the DOM in document order (pre-order). The 
+ * exceptions to this are {@code preceding}, {@code precedingSibling}, 
+ * {@code ancestor}, {@code ancestor-or-self}, {@code attribute} and 
+ * {@code namespace}. {@code preceding}, {@code precedingSibling}, 
+ * {@code ancestor} and {@code ancestor-or-self} traverse the DOM in reverse 
+ * document order. For {@code ancestor} and {@code ancestor-or-self} this means
+ * we climb up the DOM. The order for {@code attribute} and {@code namespace} is
+ * not guaranteed and the order should be considered arbitrary. 
+ * 
+ * If at anytime during the traversal the callback function returns false, and 
+ * only false (other values that evaluate to false, like null or '', won't 
+ * affect the traversal), the guide will stop the traversal at that node and 
+ * return false. Otherwise, if the traversal completes normally, the guide 
+ * returns true.
+ *
+ * @note When there are axes who have dashes (-) in the name, there is also a
+ * corresponding guide with the dashes removed and camel-cased so it can be used
+ * as an object method (ie. with a .). For example, there is a guide 
+ * {@code guide['descendant-or-self'] and, also, {@code guide.descendantOrSelf}.
+ *
+ * @note The Guide is meant to be subclassed (or an instance extended) to 
+ * support new axes, should an implementation require it.
+ */
+xpath.core.AxisGuide = xpath.util.Class({
+    init: function() { },
+    self: function(n, cb) {
+        return cb(n) !== false;
+    },
+    parent: function(n, cb) {
+        if (!n.parentNode)
+            return true;
+        return cb(n.parentNode) !== false;
+    },
+    child: function(n, cb) {
+        var kids = n.childNodes;
+        for (var i = 0, len = kids.length; i < len; i++)
+            if (cb(kids[i]) === false)
+                return false;
+        return true;
+    },
+    followingSibling: function(n, cb) {
+        while (n = n.nextSibling)
+            if (cb(n) === false)
+                return false;
+        return true;
+    },
+    precedingSibling: function(n, cb) {
+        while (n = n.previousSibling)
+            if (cb(n) === false)
+                return false;
+        return true;
+    },
+    ancestor: function(n, cb) {
+        for (n = n.parentNode; n; n = n.parentNode)
+            if (cb(n) === false)
+                return false;
+        return true;
+    },
+    descendant: function(n, cb) {
+        var nodeStack = Array.prototype.slice.call(n.childNodes).reverse();
+        for (n = nodeStack.pop(); n !== undefined; n = nodeStack.pop()) {
+            if (cb(n) === false)
+                return false;
+            for (var kids = n.childNodes, i = kids.length - 1; i >= 0; i--)
+                nodeStack.push(kids[i]);
+        }
+        return true;
+    },
+    following: function(n, cb) {
+        for (; n; n = n.parentNode) {
+            if (this.followingSibling(n, function(sib) {
+                        return this.descendant-or-self(sib);
+                    }) === false)
+                return false;
+        }
+        return true;
+    },
+    preceding: function(n, cb) {
+        for (; n; n = n.parentNode) {
+            if (this.precedingSibling(n, function(sib) {
+                        return this.reverseOrderDescendant(sib, cb);
+                    }) === false)
+                return false;
+        }
+        return true;
+    },
+    ancestorOrSelf: function(n, cb) {
+        return cb(n) !== false && this.ancestor(n, cb) !== false;
+    },
+    descendantOrSelf: function(n, cb) {
+        return cb(n) !== false && this.descendant(n, cb) !== false;
+    },
+    attribute: function(n, cb) {
+        // Only ELEMENT node types have attributes
+        if (n.nodeType == 1) {
+            var attrs = n.attributes;   // attrs is a NamedNodeMap
+            for (var i = 0, len = attrs.length; i < len; i++)
+                if (cb(attrs[i]) === false)
+                    return false;
+        }
+        return true;
+    },
+    namespace: function(n, cb) {
+        /// @todo Write namespace guide
+        return false;
+    },
+    reverseOrderDescendant: function(n, cb) {
+        /// @todo Re-write this function to be iterative, not recursive...
+        
+        if (n.hasChildNodes())
+            for (var k = n.lastChild; k; k = k.previousSibling)
+                if (this.reverseDescendant(k) === false)
+                    return false;
+        return cb(n) !== false;
+    },
+    'following-sibling': function(n, cb) { return this.followingSibling(n, cb); },
+    'preceding-sibling': function(n, cb) { return this.precedingSibling(n, cb); },
+    'ancestor-or-self': function(n, cb) { return this.ancestorOrSelf(n, cb); },
+    'descendant-or-self': function(n, cb) { return this.descendantOrSelf(n, cb); }
+});
+
 })();
--- src/xpath.interpreter.js
+++ src/xpath.interpreter.js
@@ -76,7 +76,7 @@ var EvaluationContext = xpath.interpreter.EvaluationContext = Class({
             this.document = this.item.ownerDocument;
         
         // Separates the axis guide from the interpreter implementation
-        this.axisGuide = new xpath.interpreter.AxisGuide();
+        this.axisGuideManager = new xpath.core.AxisGuideManager();
     },
     
     
@@ -222,13 +222,7 @@ var EvaluationContext = xpath.interpreter.EvaluationContext = Class({
       * @return A function that can be used to traverse an axis for a node
       */
      getAxisGuide: function(axis) {
-        if (!this.axisGuide[axis])
-            return null;
-
-        var axisGuide = this.axisGuide;
-        return function(n, cb) {
-            axisGuide[axis](n, cb);
-        };
+        return this.axisGuideManager.getAxisGuide(axis);
      },
      
      
@@ -396,9 +390,9 @@ var XPathInterpreter = xpath.interpreter.Interpreter = Class(xpath.ast.ASTVisito
                 // Update the size and context iterator.
                 context.size = count;
                 context.iter = function(cb) {
-                        var count = 1;
+                        var pos = 1;
                         return guide(n, function(n) {
-                                context.pos = count++;
+                                context.pos = pos++;
                                 context.item = n;
                                 return cb(context.item, context.pos);
                             });
@@ -708,145 +702,6 @@ var XPathInterpreter = xpath.interpreter.Interpreter = Class(xpath.ast.ASTVisito
         var result = this.resultStack.pop();
         this.resultStack.push(this.context.call("negate", this.resultStack.pop()));
     }
-});
-
-
-// getElementsByTagName could substantially speed up a search in cases where the
-// axes involves a large number of nodes, but getElementsByTagName returns a 
-// small set. Either way we have to iterate through all of either set, so, if
-// the NodeList returned by getElementsByTagName is smaller then the number of 
-// nodes searched in the guided traversal, then we should use 
-// getElementsByTagName... Of course, we also run the problem where if we have
-// a position given (eg. div[2]), then it would be faster to use the the guide.
-// I think this kind of optimization should be abstracted. Perhaps create a 
-// GuideManager that returns a "good" guide to use, given some parameters (eg.
-// predicate list, name test, axis, etc.)
-
-
-/**
- * Axis guides are methods that take 2 arguments; a node and a callback. A
- * guide for an axis will traverse, in order, all the nodes in that axis, each
- * time calling the callback provided with the current node as the argument.
- *
- * Generally, we traverse the DOM in document order (pre-order). The 
- * exceptions to this are {@code preceding}, {@code precedingSibling}, 
- * {@code ancestor}, {@code ancestor-or-self}, {@code attribute} and 
- * {@code namespace}. {@code preceding}, {@code precedingSibling}, 
- * {@code ancestor} and {@code ancestor-or-self} traverse the DOM in reverse 
- * document order. For {@code ancestor} and {@code ancestor-or-self} this means
- * we climb up the DOM. The order for {@code attribute} and {@code namespace} is
- * not guaranteed and the order should be considered arbitrary. 
- * 
- * If at anytime during the traversal the callback function returns false, and 
- * only false (other values that evaluate to false, like null or '', won't 
- * affect the traversal), the guide will stop the traversal at that node and 
- * return false. Otherwise, if the traversal completes normally, the guide 
- * returns true.
- *
- * @note When there are axes who have dashes (-) in the name, there is also a
- * corresponding guide with the dashes removed and camel-cased so it can be used
- * as an object method (ie. with a .). For example, there is a guide 
- * {@code guide['descendant-or-self'] and, also, {@code guide.descendantOrSelf}.
- *
- * @note The Guide is meant to be subclassed (or an instance extended) to 
- * support new axes, should an implementation require it.
- */
-xpath.interpreter.AxisGuide = Class({
-    init: function() { },
-    self: function(n, cb) {
-        return cb(n) !== false;
-    },
-    parent: function(n, cb) {
-        if (!n.parentNode)
-            return true;
-        return cb(n.parentNode) !== false;
-    },
-    child: function(n, cb) {
-        var kids = n.childNodes;
-        for (var i = 0, len = kids.length; i < len; i++)
-            if (cb(kids[i]) === false)
-                return false;
-        return true;
-    },
-    followingSibling: function(n, cb) {
-        while (n = n.nextSibling)
-            if (cb(n) === false)
-                return false;
-        return true;
-    },
-    precedingSibling: function(n, cb) {
-        while (n = n.previousSibling)
-            if (cb(n) === false)
-                return false;
-        return true;
-    },
-    ancestor: function(n, cb) {
-        for (n = n.parentNode; n; n = n.parentNode)
-            if (cb(n) === false)
-                return false;
-        return true;
-    },
-    descendant: function(n, cb) {
-        var nodeStack = Array.prototype.slice.call(n.childNodes).reverse();
-        for (n = nodeStack.pop(); n !== undefined; n = nodeStack.pop()) {
-            if (cb(n) === false)
-                return false;
-            for (var kids = n.childNodes, i = kids.length - 1; i >= 0; i--)
-                nodeStack.push(kids[i]);
-        }
-        return true;
-    },
-    following: function(n, cb) {
-        for (; n; n = n.parentNode) {
-            if (this.followingSibling(n, function(sib) {
-                        return this.descendant-or-self(sib);
-                    }) === false)
-                return false;
-        }
-        return true;
-    },
-    preceding: function(n, cb) {
-        for (; n; n = n.parentNode) {
-            if (this.precedingSibling(n, function(sib) {
-                        return this.reverseOrderDescendant(sib, cb);
-                    }) === false)
-                return false;
-        }
-        return true;
-    },
-    ancestorOrSelf: function(n, cb) {
-        return cb(n) !== false && this.ancestor(n, cb) !== false;
-    },
-    descendantOrSelf: function(n, cb) {
-        return cb(n) !== false && this.descendant(n, cb) !== false;
-    },
-    attribute: function(n, cb) {
-        // Only ELEMENT node types have attributes
-        if (n.nodeType == 1) {
-            var attrs = n.attributes;   // attrs is a NamedNodeMap
-            for (var i = 0, len = attrs.length; i < len; i++)
-                if (cb(attrs[i]) === false)
-                    return false;
-        }
-        return true;
-    },
-    namespace: function(n, cb) {
-        /// @todo Write namespace guide
-        return false;
-    },
-    reverseOrderDescendant: function(n, cb) {
-        /// @todo Re-write this function to be iterative, not recursive...
-        
-        if (n.hasChildNodes())
-            for (var k = n.lastChild; k; k = k.previousSibling)
-                if (this.reverseDescendant(k) === false)
-                    return false;
-        return cb(n) !== false;
-    },
-    'following-sibling': function(n, cb) { return this.followingSibling(n, cb); },
-    'preceding-sibling': function(n, cb) { return this.precedingSibling(n, cb); },
-    'ancestor-or-self': function(n, cb) { return this.ancestorOrSelf(n, cb); },
-    'descendant-or-self': function(n, cb) { return this.descendantOrSelf(n, cb); }
 });
 
 
